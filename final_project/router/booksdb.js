df7754133d53fc8b3f637c68cc9af0d59307f9c9
Andrew Yoon <ayoon@clemson.edu>
2024-07-14 17:05:50 -0400

    complete Book reviews asg

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "isbn": "0141023384","reviews": {}},
+      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "isbn": "0140449016","reviews": {} },
+      3: {"author": "Dante Alighieri","title": "The Divine Comedy","isbn": "0140448958", "reviews": {} },
+      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "isbn": "0140449199","reviews": {} },
+      5: {"author": "Unknown","title": "The Book Of Job", "isbn": "0140441872","reviews": {} },
+      6: {"author": "Unknown","title": "One Thousand and One Nights", "isbn": "0140449385","reviews": {"Joe":"Good book","Jane":"Good read","Damian":"Posted review"} },
+      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "isbn": "0140447692","reviews": {} },
+      8: {"author": "Jane Austen","title": "Pride and Prejudice", "isbn": "0141439513","reviews": {} },
+      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "isbn": "0140449717","reviews": {} },
+      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn": "0330339423","reviews": {} }
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,116 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const doesExist = (username) => {
+  // Filter the users array for any user with the same username
+  let userswithsamename = users.filter((user) => {
+      return user.username === username;
+  });
+  // Return true if any user with the same username is found, otherwise false
+  if (userswithsamename.length > 0) {
+      return true;
+  } else {
+      return false;
+  }
+}
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if both username and password are provided
+  if (username && password) {
+      // Check if the user does not already exist
+      if (!doesExist(username)) {
+          // Add the new user to the users array
+          users.push({"username": username, "password": password});
+          return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+          return res.status(404).json({message: "User already exists!"});
+      }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({message: "Unable to register user; Username or password not provided."});  
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/',async function (req, res) {
+
+   try {
+    const data = await books;
+    res.send(JSON.stringify(data, null, 4));
+       }
+    catch (error) {
+      console.error(error.toString());
+      res.status(500).json({ error: 'Internal Server Error' });
+   }
+
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn',async function (req, res) {
+  try {
+    let isbn = req.params.isbn;
+    const data = await books;
+    let filtered_isbn = Object.values(data).filter(book => book.isbn.includes(isbn));
+    if(filtered_isbn.length === 0){
+      return res.status(404).json({message: "Book with "+ filtered_isbn + " does not exist"});
+    }
+    res.send(JSON.stringify(filtered_isbn, null, 4));
+       }
+    catch (error) {
+      console.error(error.toString());
+      res.status(500).json({ error: 'Internal Server Error' });
+   }
+
+
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author',async function (req, res) {
+  try {
+    let author = req.params.author;
+    const data = await books;
+    let filtered_author = Object.values(data).filter(book => book.author.includes(author));
+    if(filtered_author.length === 0){
+      return res.status(400).json({message: "Book with "+ filtered_author + " does not exist"});
+    }
+    res.send(JSON.stringify(filtered_author, null, 4));
+       }
+    catch (error) {
+      console.error(error.toString());
+      res.status(500).json({ error: 'Internal Server Error' });
+   }
+
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title',async function (req, res) {
+  try {
+    let title = req.params.title;
+    const data = await books;
+    let filtered_title = Object.values(data).filter(book => book.title.includes(title));
+    if(filtered_title.length === 0){
+      return res.status(400).json({message: "Book with "+ filtered_title + " does not exist"});
+    }
+    res.send(JSON.stringify(filtered_title, null, 4));
+       }
+    catch (error) {
+      console.error(error.toString());
+    }
+
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn',async function (req, res) {
+  let isbn = req.params.isbn;
+  const data = await books;
+  let filtered_isbn = Object.values(data).filter(book => book.isbn.includes(isbn));
+  if(filtered_isbn.length === 0){
+    return res.status(400).json({message: "Book with "+ filtered_isbn + " does not exist"});
+  }
+
+  res.send(JSON.stringify(filtered_isbn[0].reviews, null, 4));
 });
 
 module.exports.general = public_users;
